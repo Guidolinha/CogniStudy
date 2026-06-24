6290b7c14b08a140c0a2d980520a9ba87ccd70a3
Guidolinha <gp645457@gmail.com>
2026-06-24 16:38:40 -0300

    Retirei a chave api do código

--- teste.py
+++ teste.py
@@ -1,10 +1,6 @@
 import os
 from groq import Groq
 
-# Coloque a sua chave aqui por enquanto para testar
-os.environ["GROQ_API_KEY"] = "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"
-
-
 client = Groq() # Ele lerá a chave automaticamente da variável de ambiente acima
 chat_completion = client.chat.completions.create(
     messages=[{"role": "user", "content": "Olá, tudo bem?Qual seu nome?"}],
